e1c28700aabe1565b642397c6f2ed5d2d658195a
cotatenis <cotatenis@cotatenis.com>
2022-04-02 17:11:02 +0000

    add raw_maze model

--- models/dev/raw/raw_kings.sql
+++
@@ -1,3 +0,0 @@
-with raw_kings as (
-    select * from {{ source('cotatenis_dev', 'raw_kings') }}
-) SELECT * FROM raw_kings
---
+++ models/dev/staging/sneakers/stg_kings.sql
@@ -0,0 +1,15 @@
+select 
+      payload:title::text as product
+    , payload:sku::text as sku
+    , payload:description as description
+    , payload:available::boolean as is_available
+    , payload:price::float as price
+    , payload:created_at::timestamp as created_at
+    , payload:published_at::timestamp as published_at
+    , payload:reference_first_image::text as reference_first_image
+    , payload:image_uris as image_uris
+    , payload:spider::text spider
+    , payload:spider_version::text spider_version
+    , payload:timestamp::timestamp as collected_at
+    , payload:url::text as url
+FROM {{ source('cotatenis_dev', 'raw_kings') }}
