b8903f19d8d74723d9665203ca2a06d075f03d31
netoferraz <netoferraz@gmail.com>
2022-04-04 14:34:37 -0300

    add variants column into stg_kings model

--- models/dev/staging/sneakers/stg_kings.sql
+++ models/dev/staging/sneakers/stg_kings.sql
@@ -4,6 +4,7 @@ select
     , payload:description::text as description
     , payload:available::boolean as is_available
     , payload:price::float as price
+    , payload:variants as variants
     , payload:created_at::timestamp as created_at
     , payload:published_at::timestamp as published_at
     , payload:reference_first_image::text as reference_first_image
